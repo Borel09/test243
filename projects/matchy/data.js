d0e437ad45dd4083e046ccd69ebf85597d60e97e
Ryan Borel <rjborel@gmail.com>
2020-09-21 21:12:04 +0000

    matchy1

---
+++ projects/matchy/data.js
@@ -0,0 +1,123 @@
+/**
+ * Part 1
+ *
+ * In this file, we're going to practice
+ * creating and accessing data structues.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Object Creation //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal = {};
+animal.species = 'Dog';
+animal['name'] = 'Cowboy';
+animal.noises = [];
+console.log(animal);
+
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Array Creation ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var noises = [];
+noises[0] = 'Bark';
+noises.push('Boof');
+noises.unshift('Bork');
+noises[noises.length] = 'Woof';
+
+console.log(noises.length);
+console.log(noises[noises.length-1]);
+console.log(noises);
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Combining Step 1 and 2 ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('Hey there Human.');
+console.log(noises);
+
+
+/* *******************************************************************
+ * Step 4 - Review
+ *
+ * 1. What are the different ways you can access properties on objects?
+ *          A: you can access properties through either bracket notation or dot notation
+ * 2. What are the different ways of accessing elements on arrays?
+            A: you can only use bracket notation on arrays
+ *
+ * *******************************************************************
+ */
+
+/* *******************************************************************
+ * Step 5 - Take a Break!
+ *
+ * It's super important to give your brain and yourself
+ * a rest when you can! Grab a drink and have a think!
+ * For like 10 minutes, then, BACK TO WORK! :)
+ * *******************************************************************
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 6 - A Collection of Animals //////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animals = [];
+animals.push(animal);
+
+var duck = { 
+    species: 'duck',
+    name: 'Jerome',
+    noises: ['quack', 'honk', 'sneeze', 'woosh']
+    };
+animals.push(duck);
+
+var cow = {
+    species: 'cow',
+    name: 'Brendan Fraser',
+    noises: ['moo', 'moo again', "I'm a cow"]
+};
+animals.push(cow);
+
+var cat = {
+    species: 'feline',
+    name: 'Harold',
+    noises: ['meow', 'purr', "I'll knock your stuff over"]
+};
+animals.push(cat);
+
+console.log(animals.length);
+
+//////////////////////////////////////////////////////////////////////
+// Step 7 - Making Friends ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var friends = []; //using array because this is a simple list not a collection.
+
+function getRandom(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+friends.push(getRandom(animals).name);
+console.log(friends);
+
+animal['friends'] = friends;
+
+
+/**
+ * Nice work! You're done Part 1. Pat yourself on the back and
+ * move onto Part 2 in the file called "functions.js"
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal, noises, animals, friends, getRandom;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+     module.exports.animal = animal || null;
+     module.exports.noises = noises || null;
+     module.exports.animals = animals || null;
+     module.exports.friends = friends || null;
+     module.exports.getRandom = getRandom || null;
+}
---
+++ studies/datatypes.js
@@ -0,0 +1,21 @@
+/**
+ * DATATYPES: Understanding the different value types used in JavaScript is important because it allows us to
+ *be able to manipulate values and data within a program. JavaScript uses six main data types and 
+ *can be seperated into three different categories: Primitive and Complex. Primitive data types refer
+ *to the fact that variables have a fixed amount of memory, however, complex data types do not have 
+ *a fixed size. Therefore, primitive datatypes store data by copying values and complex data types
+ *store data through copying by reference.
+ *
+ *There are three primitive data types: strings, numbers, and bolean values. 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
