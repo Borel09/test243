c682146bc092924dbe17fe4975ca116d4d7925f8
Ryan Borel <rjborel@gmail.com>
2020-10-17 13:55:25 +0000

    EQJS

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,32 +2,76 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
-
+function flatten(arrays) {
+ let newArray = arrays.reduce(function(prev, curr){
+   return prev.concat(curr);
+ });
+ return newArray;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
-
+function loop(start, test, update, body) {
+  for(let value = start; test(value); value = update(value)){
+    body(value);
+  }
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
+function every(array, test) {
+  return !array.some(ele => !test(ele))
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => code >= from &&
+                                           code < to)) {
+      return script;
+    }
+  }
+  return null;
+}
 
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function dominantDirection(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+  switch (scripts.length) {
+    case 0:
+      return 'no dominant direction found';
+    case 1:
+      return scripts[0].name;
+    default:
+      if (scripts.reduce((acc, cur) => acc.count === cur.count)) {
+        return 'no dominant direction found';
+      } else {
+        return scripts.reduce((acc, cur) => acc.count >= cur.count ? acc.name : cur.name);
+      }
+  }
 }
 
 // /////////////////////////////////////////////////////////////////////////////
